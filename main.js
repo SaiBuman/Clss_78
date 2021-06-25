ba98c1c71fc1007850e6bf37391b39419567d166
Sai Buman <78092854+SaiBuman@users.noreply.github.com>
2021-06-25 21:04:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var student_array =[];
+function submit() {
+    var name1 = document.getElementById("name_of_student_1").value ;
+    var name2 = document.getElementById("name_of_student_2").value ;
+    var name3 = document.getElementById("name_of_student_3").value ;
+    var name4 = document.getElementById("name_of_student_4").value ;
+    student_array.push(name1) ;
+    student_array.push(name2) ;
+    student_array.push(name3) ;
+    student_array.push(name4) ;
+    console.log(student_array) ;
+    document.getElementById("display_name").innerHTML=student_array ;
+    document.getElementById("submit_button").style.display="none" ;
+    document.getElementById("sort_button").style.display="inline-block" ;
+}
+function sorting(){
+    student_array.sort() ;
+    console.log(student_array) ;
+    document.getElementById("display_name").innerHTML=student_array ;
+} 
+
